b139d3925bb9f452bed7fe3d9db238e6aefb1820
tatul <tatul.kalashyan@gmail.com>
2019-05-29 11:39:15 +0400

    #boards-routing-changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
-import Boards from './components/Boards';
+import React, {Component} from 'react';
+import Boards from './components/Boards/Boards';
 import './App.css';
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Boards />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <Route exact path="/" component={Boards}/>
+                {/*<Route path="/list/:name/:id" component={ListComponent}/>*/}
+            </Router>
+
+        );
+    }
 }
 
 export default App;
