3d2cc66dcf47081657c077214c7c87b3bdc027b2
Levon Parazyan <levondp93@gmail.com>
2019-06-12 15:51:25 +0400

    added localStorage

---
+++ src/components/Boards/constBoards.js
@@ -0,0 +1,24 @@
+const Board = [
+    {
+        id: 1,
+        name: ''
+    },
+    {
+        id: 2,
+        name: ''
+    },
+    {
+        id: 3,
+        name: ''
+    },
+    {
+        id: 4,
+        name: ''
+    },
+    {
+        id: 5,
+        name: ''
+    }
+];
+
+export default Board;
