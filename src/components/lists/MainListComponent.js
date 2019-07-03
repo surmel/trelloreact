58f74af9016a583e91e6a18c2eca66ea9f164cc5
Suren <surmel9@gmail.com>
2019-07-03 20:12:05 +0400

    style corrections

--- src/components/lists/MainListComponent.js
+++ src/components/lists/MainListComponent.js
@@ -5,21 +5,32 @@ import ListsComponent from "../../containers/lists/listsComponent";
 
 const styles = {
     container: {
-        padding: '10px'
+        padding: '10px',
+        marginTop: '45px'
     },
+    mainListContainer: {
+        position: 'absolute',
+        height: '100%',
+        width: '100%',
+        left: '0',
+        top: '0',
+        zIndex: '-1'
+    }
 };
 
 class MainListComponent extends React.Component {
 
     render() {
         return (
-            <div style={styles.container}>
-                <div className='clearfix'>
-                    <MenuComponent/>
-                </div>
-                <div className='container-fluid'>
-                    <div className='row'>
-                        <ListsComponent />
+            <div id='mainListBackground' style={styles.mainListContainer}>
+                <div style={styles.container}>
+                    <div className='clearfix'>
+                        <MenuComponent/>
+                    </div>
+                    <div className='container-fluid'>
+                        <div className='row'>
+                            <ListsComponent/>
+                        </div>
                     </div>
                 </div>
             </div>
